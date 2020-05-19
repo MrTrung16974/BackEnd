20cdff4bea7809249e309819103b12975f7f4c15
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-05-19 15:05:14 +0700

    update

--- mongodb/src/main/java/com/example/mongodb/MongodbApplication.java
+++ mongodb/src/main/java/com/example/mongodb/MongodbApplication.java
@@ -8,14 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongodbApplication  {
-//  implements CommandLineRunner
+public class MongodbApplication {
+//        implements CommandLineRunner {
+
     public static void main(String[] args) {
         SpringApplication.run(MongodbApplication.class, args);
     }
 
-//    @Autowired
-//    ProductRepository productRepository;
+    @Autowired
+    ProductRepository productRepository;
 
 //    @Override
 //    public void  run(String...args) throws Exception {
@@ -24,6 +25,7 @@ public class MongodbApplication  {
 //            p.setName("Name " + i);
 //            p.setPrice((double)i*100);
 //            p.setNumber(i);
+//            p.setId(""+i);
 //            productRepository.save(p);
 //        }
 //    }
