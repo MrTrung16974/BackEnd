d34dac9a9efc312205585f2a0cd246a1b41cafbe
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-05-29 22:44:11 +0700

    update

--- springsecurity/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
+++ springsecurity/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
         http.authorizeRequests().mvcMatchers("/home**").permitAll();
         http.authorizeRequests().mvcMatchers("/register", "/catalog**").permitAll();
-        http.authorizeRequests().mvcMatchers("/order").access("hasAnyAuthority('USER')");
+        http.authorizeRequests().mvcMatchers("/cart", "/checkout").access("hasAnyAuthority('USER')");
         http.authorizeRequests().and().formLogin().loginPage("/login")
                 .loginProcessingUrl("/login")
                 .failureUrl("/login?error=true")
--- springsecurity/src/main/resources/static/js/scripts.js
+++ springsecurity/src/main/resources/static/js/scripts.js
@@ -1,4 +1,5 @@
 $( document ).ready(function () {
+    var pathname = window.location.pathname; // Returns path only (/path/example.html)
     console.log($($(".main-content-wrapper .header-area .amado-nav>li")));
     $(".main-content-wrapper .header-area .amado-nav>li").click(function () {
         console.log("Ok");
