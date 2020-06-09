03ad08f56b51e6aff5e995956ab62e4e41f6707d
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-06-09 13:30:20 +0700

    update

--- mongodb/src/main/java/com/example/mongodb/controller/HomeController.java
+++ mongodb/src/main/java/com/example/mongodb/controller/HomeController.java
@@ -2,17 +2,18 @@ package com.example.mongodb.controller;
 
 import com.example.mongodb.dto.BaseResponse;
 import com.example.mongodb.model.Product;
+import com.example.mongodb.model.User;
 import com.example.mongodb.repository.OrderRepository;
 import com.example.mongodb.repository.ProductRepository;
+import com.example.mongodb.repository.UserRepository;
 import com.example.mongodb.services.OrderServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,10 +24,10 @@ public class HomeController {
     ProductRepository productRepository;
 
     @Autowired
-    OrderRepository orderRepository;
+    PasswordEncoder passwordEncoder;
 
     @Autowired
-    OrderServices orderServices;
+    UserRepository userRepository;
 
     @RequestMapping("/index")
     public String index(Model model){
@@ -35,4 +36,15 @@ public class HomeController {
         return "index";
     }
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String AddUser(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          @RequestParam("name") String name) {
+        User user = new User();
+        user.setId(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRoles(Arrays.asList("ADMIN", "USER", "VIP"));
+        userRepository.save(user);
+        return "redirect:/login?regisSuccess=true";
+    }
 }
