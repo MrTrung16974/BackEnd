703c00e7213674ce40a8a2fc48d0385b4f9acf5b
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-06-09 13:24:11 +0700

    remove uploadimage

--- uploadimage/src/main/java/com/example/uploadimage/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.uploadimage.repository;
-
-import com.example.uploadimage.model.Image;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends MongoRepository<Image,String> {
-}
